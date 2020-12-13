015306a979d948bf494527b6888fb5401a7c3f2d
cron <cron@localhost>
2020-12-13 22:04:07 +0900

    Remove String if-else

--- src/cron/calendar/Calendar.java
+++ src/cron/calendar/Calendar.java
@@ -1,10 +1,37 @@
 package cron.calendar;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class Calendar {
 
 	private static final int[] MAX_DAYS = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int[] LEAP_MAX_DAYS = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
+	private HashMap <Date, String>planMap;
+	
+	public Calendar() {
+		planMap = new HashMap<Date, String>();
+	}
+	
+	/**
+	 * 
+	 * @param date: "2017-06-20"
+	 * @param plan
+	 * @throws ParseException 
+	 */
+	public void registerPlan(String strDate, String plan) throws ParseException {
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+		planMap.put(date, plan);
+	}
+	
+	public String searchPlan(String strDate) throws ParseException {
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+		String plan = planMap.get(date);
+		return plan;
+	}
+	
 	public boolean isLeapYear(int year) {
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
 			return true;
@@ -63,7 +90,7 @@ public class Calendar {
 
 	private int getWeekDay(int year, int month, int day) {
 		int syear = 1970;
-		final int STANDARD_WEEKDAY = 3; // 1970/Jan/1st = Thursday
+		final int STANDARD_WEEKDAY = 4; // 1970/Jan/1st = Thursday
 		
 		int count = 0;
 		
@@ -78,19 +105,22 @@ public class Calendar {
 			count += delta;
 		}
 		
-		count += day;
+		count += day - 1;
 		
 		int weakday = (count + STANDARD_WEEKDAY) % 7;
 		return weakday;
 	}
 	
 	// simple test code here
-	public static void main(String[] args) {
-		Calendar c = new Calendar();
-		System.out.println(c.getWeekDay(1970, 1, 1) == 3);
-		System.out.println(c.getWeekDay(1971, 1, 1) == 4);
-		System.out.println(c.getWeekDay(1972, 1, 1) == 5);
-		System.out.println(c.getWeekDay(1973, 1, 1) == 0);
-		System.out.println(c.getWeekDay(1974, 1, 1) == 1);
+	public static void main(String[] args) throws ParseException {
+		Calendar cal = new Calendar();
+		System.out.println(cal.getWeekDay(1970, 1, 1) == 4);
+		System.out.println(cal.getWeekDay(1971, 1, 1) == 5);
+		System.out.println(cal.getWeekDay(1972, 1, 1) == 6);
+		System.out.println(cal.getWeekDay(1973, 1, 1) == 1);
+		System.out.println(cal.getWeekDay(1974, 1, 1) == 2);
+		
+		cal.registerPlan("2017-06-23", "Let's eat beef!");
+		System.out.println(cal.searchPlan("2017-06-23").equals("Let's eat beef!"));
 	}
 }
