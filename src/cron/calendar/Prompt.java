36733e6c45e470930290a8915067a9089eadcc0a
cron <cron@localhost>
2020-12-13 22:40:24 +0900

    add PlanItem class

--- src/cron/calendar/Prompt.java
+++ src/cron/calendar/Prompt.java
@@ -19,7 +19,7 @@ public class Prompt {
 	 * @return 0 ~ 6 (0 = Sunday, 6 = Satureday)
 	 */
 	public int parseDay(String week) {
-		switch(week) {
+		switch (week) {
 		case "su":
 			return 0;
 		case "mo":
@@ -41,14 +41,14 @@ public class Prompt {
 
 	public void runPrompt() throws ParseException {
 		printMenu();
-		
+
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		boolean isLoop = true;
 		while (isLoop) {
 			System.out.println("명령 (1, 2, 3, h, q)");
 			String cmd = scanner.next();
-			switch(cmd) {
+			switch (cmd) {
 			case "1":
 				cmdRegister(scanner, cal);
 				break;
@@ -93,14 +93,14 @@ public class Prompt {
 		System.out.println("[일정 검색]");
 		System.out.println("날짜를 입력해 주세요 (yyyy-MM-dd).");
 		String date = s.next();
-		String plan = "";
-		try {
-			plan = c.searchPlan(date);
-		} catch (ParseException e) {
-			e.printStackTrace();
-			System.err.println("일정 검색 중 오류가 발생했습니다.");
+		PlanItem plan;
+		plan = c.searchPlan(date);
+
+		if(plan != null) {
+			System.out.println(plan.detail);
+		} else {
+			System.out.println("일정이 없습니다.");
 		}
-		System.out.println(plan);
 
 	}
 
@@ -110,13 +110,13 @@ public class Prompt {
 		String date = s.next();
 		String text = "";
 		System.out.println("일정을 입력해 주세요. (문장의 끝에 ; 을 입력해 주세요.");
-		while(true) {
-			String word = s.next();
+		String word;
+		while (!(word = s.next()).endsWith(";")) {
 			text += word + " ";
-			if (word.endsWith(";")) {
-				break;
-			}
 		}
+		word = word.replace(";", "");
+		text += word;
+		
 		c.registerPlan(date, text);
 	}
 
